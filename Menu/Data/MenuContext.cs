de1e259eab782777bbe7c2baf6abc9c535e01ac5
Айдин Нурланбеков <nurlanbekovich.a@gmail.com>
2024-03-29 00:43:20 +0600

    Добавьте файлы проекта.

---
+++ Menu/Data/MenuContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Menu.Models;
+using Microsoft.Identity.Client;
+
+namespace Menu.Data
+{
+    public class MenuContext : DbContext
+    {
+        public MenuContext(DbContextOptions<MenuContext> options) :base(options) 
+        { 
+        
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DishIngredient>().HasKey(di=>new
+            {
+                di.DishId,
+                di.IngredeientId    
+            });
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.DishIngredients).HasForeignKey(d => d.DishId);
+            modelBuilder.Entity<DishIngredient>().HasOne(i => i.Ingredient).WithMany(di => di.DishIngredients).HasForeignKey(i => i.IngredeientId);
+            modelBuilder.Entity<Dish>().HasData(new Dish { Id = 1, Name="Маргарита", Price=550, ImageUrl= "https://www.google.com/imgres?imgurl=https%3A%2F%2Flh3.googleusercontent.com%2F-F7-f2RyixFJ_0-MIGehlz7lp08CkWuy7Y64qDx8zcSrAyHA_uWVnJx1XOVAHg_qoFD7fW34aWScKlOz7tlHx8LeBxDoB64vaZ6LCKKMAPPnr8-QTpPpQVVK-xGPWFZomSVkVZXW&tbnid=KRWx8o3QDyUbEM&vet=12ahUKEwjFoouO05CFAxVlMhAIHUy2CzEQMygDegQIARBX..i&imgrefurl=https%3A%2F%2Fpizza.od.ua%2Fblog%2Fpitstsa-margarita-korolevskaya-pishcha-ili-eda-dlya-bednyakov.html&docid=UrQT4GHwqiqLjM&w=624&h=416&q=%D0%BC%D0%B0%D1%80%D0%B3%D0%B0%D1%80%D0%B8%D1%82%D0%B0%20%D0%BF%D0%B8%D1%86%D1%86%D0%B0&ved=2ahUKEwjFoouO05CFAxVlMhAIHUy2CzEQMygDegQIARBX" });
+            modelBuilder.Entity<Ingredient>().HasData(new Ingredient { Id = 1, Name = "Сыр" });
+            modelBuilder.Entity<Ingredient>().HasData(new Ingredient { Id = 2, Name = "Томатный соус" });
+            modelBuilder.Entity<DishIngredient>().HasData(
+                new DishIngredient { DishId = 1, IngredeientId = 1 },
+                new DishIngredient { DishId = 1, IngredeientId = 2 }
+                );
+           
+
+            base.OnModelCreating(modelBuilder);
+            
+        }
+        public DbSet<Dish> Dishes { get; set; } 
+        public DbSet<Ingredient> Ingredients { get; set;}
+        public DbSet<DishIngredient> DishIngredients { get; set; }
+
+    }
+}
+    
---
+++ Menu/Migrations/20240328173514_Initial migration.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Menu.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DishIngredients",
+                columns: table => new
+                {
+                    DishId = table.Column<int>(type: "int", nullable: false),
+                    IngredeientId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DishIngredients", x => new { x.DishId, x.IngredeientId });
+                    table.ForeignKey(
+                        name: "FK_DishIngredients_Dishes_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DishIngredients_Ingredients_IngredeientId",
+                        column: x => x.IngredeientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dishes",
+                columns: new[] { "Id", "ImageUrl", "Name", "Price" },
+                values: new object[] { 1, "https://www.google.com/imgres?imgurl=https%3A%2F%2Flh3.googleusercontent.com%2F-F7-f2RyixFJ_0-MIGehlz7lp08CkWuy7Y64qDx8zcSrAyHA_uWVnJx1XOVAHg_qoFD7fW34aWScKlOz7tlHx8LeBxDoB64vaZ6LCKKMAPPnr8-QTpPpQVVK-xGPWFZomSVkVZXW&tbnid=KRWx8o3QDyUbEM&vet=12ahUKEwjFoouO05CFAxVlMhAIHUy2CzEQMygDegQIARBX..i&imgrefurl=https%3A%2F%2Fpizza.od.ua%2Fblog%2Fpitstsa-margarita-korolevskaya-pishcha-ili-eda-dlya-bednyakov.html&docid=UrQT4GHwqiqLjM&w=624&h=416&q=%D0%BC%D0%B0%D1%80%D0%B3%D0%B0%D1%80%D0%B8%D1%82%D0%B0%20%D0%BF%D0%B8%D1%86%D1%86%D0%B0&ved=2ahUKEwjFoouO05CFAxVlMhAIHUy2CzEQMygDegQIARBX", "Маргарита", 550m });
+
+            migrationBuilder.InsertData(
+                table: "Ingredients",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Сыр" },
+                    { 2, "Томатный соус" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "DishIngredients",
+                columns: new[] { "DishId", "IngredeientId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DishIngredients_IngredeientId",
+                table: "DishIngredients",
+                column: "IngredeientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DishIngredients");
+
+            migrationBuilder.DropTable(
+                name: "Dishes");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+        }
+    }
+}
